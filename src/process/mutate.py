ca6338a26418bd29f4022a68a50b545bdee101c7
carrascomj <carrascomurielj@gmail.com>
2020-08-27 10:57:58 +0200

    docs: complete types

--- src/process/mutate.py
+++ src/process/mutate.py
@@ -4,12 +4,12 @@ author: Jorge Carrasco Muriel
 contact: carrascomurielj@gmail.com
 """
 from os.path import join, pardir
-from typing import Dict
+from typing import Tuple, Dict
 
 import pandas as pd
 
 
-def mutate(parent: str, sites: Dict):
+def mutate(parent: str, sites: Dict) -> str:
     """Mutate a parent string given the sites."""
     child = list(parent)
     for site, letter in sites.items():
@@ -23,17 +23,24 @@ def write_fasta(path: str, header: str, seq: str):
         f.write(f">{header}\n{seq}\n")
 
 
-def _transform_row(row):
+def _transform_row(row: pd.Series) -> Tuple[str, Dict]:
     mut_dict = row[~row.isna()].to_dict()
     id = mut_dict["ID"]
     del mut_dict["ID"]
     return id, mut_dict
 
 
-def mutate_all(parent_light: str, parent_heavy: str, mut_light: pd.DataFrame,
-               mut_heavy: pd.DataFrame, data_path: str, separate=False):
+def mutate_all(
+    parent_light: str,
+    parent_heavy: str,
+    mut_light: pd.DataFrame,
+    mut_heavy: pd.DataFrame,
+    data_path: str,
+    separate: bool = False,
+    out_path: str = "all_variants.fasta",
+):
     """Apply mutations to parent chains."""
-    fasta = join(data_path, "all_variants.fasta")
+    fasta = join(data_path, out_path)
     for r_light, r_heavy in zip(mut_light.iterrows(), mut_heavy.iterrows()):
         id, mut_dict = _transform_row(r_light[1])
         if separate:
@@ -43,16 +50,16 @@ def mutate_all(parent_light: str, parent_heavy: str, mut_light: pd.DataFrame,
         write_fasta(fasta, f"{id}_H", mutate(parent_heavy, mut_dict))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     data = join(pardir, pardir, "data", "processed")
     with open(join(data, pardir, "raw", "parent.fasta")) as f:
         lines = f.readlines()
         parent = lines[1]
         parent_light = lines[3]
-        print(parent, parent_light, sep="\n")
     mutate_all(
-        parent_light, parent,
+        parent_light,
+        parent,
         pd.read_csv(join(data, "light_chains.tsv"), sep="\t"),
         pd.read_csv(join(data, "heavy_chains.tsv"), sep="\t"),
-        join(data, "fasta")
+        join(data, "fasta"),
     )
