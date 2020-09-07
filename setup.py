e1ab66f3b145956492ce2c947d4198ce76ba6ffa
carrascomj <carrascomurielj@gmail.com>
2020-09-07 11:00:59 +0200

    feat: set repo as python module

--- setup.py
+++ setup.py
@@ -3,20 +3,19 @@ from setuptools import setup, find_packages
 
 
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
 
 setup(
-    name='nnant',
-    version='0.2.5',
-    description='NNETS lundbeck antibodies',
-    author='Jorge Carrasco Muriel',
-    author_email='jocc@lundbeck.com',
-    keywords='python metabolic-models bioinformatics antibodies',
+    name="nnbody",
+    package_dir="src",
+    version="0.1.0",
+    description="NNETS lundbeck antibodies",
+    author="Jorge Carrasco Muriel",
+    author_email="jocc@lundbeck.com",
+    keywords="python metabolic-models bioinformatics antibodies",
     packages=find_packages(),
-    install_requires=[
-        "pandas", "torch", "torchvision", "matplotlib"
-    ],
-    zip_safe=False
+    install_requires=["pandas", "torch", "torchvision", "matplotlib"],
+    zip_safe=False,
 )
